4b5fd4cdf7cdc2c2287a0b579b96f8ab4bb5e7fb
shantanukumar22 <justforworkandstudy@gmail.com>
2025-11-14 23:47:09 +0530

    sdf

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -6,17 +6,21 @@ from src.nodes.case_Node import CaseNode
 class GraphBuilder():
     def __init__(self,llm):
         self.llm=llm
-        self.graph=StateGraph(CaseState)
+
     def build_topic_graph(self):
-        
+
         """Build a graph to give the judgements based on the topic"""
+        self.graph = StateGraph(CaseState)
         self.case_node_obj=CaseNode(self.llm)
-        
+        self.graph.add_node("ipc_sections",self.case_node_obj.ipc_sections_creation)
         self.graph.add_node("title_creation",self.case_node_obj.title_creation)
         self.graph.add_node("judgement",self.case_node_obj.charges_creation)
+        self.graph.add_node("final_sentence",self.case_node_obj.final_sentence)
         self.graph.add_edge(START,"title_creation")
-        self.graph.add_edge("title_creation","judgement")
-        self.graph.add_edge("judgement",END)
+        self.graph.add_edge("title_creation","ipc_sections")
+        self.graph.add_edge("ipc_sections","judgement")
+        self.graph.add_edge("judgement","final_sentence")
+        self.graph.add_edge("final_sentence",END)
         return self.graph
         
     def setup_graph(self,usecase):
@@ -24,4 +28,3 @@ class GraphBuilder():
             self.build_topic_graph()
 
         return self.graph.compile()
-    
--- src/nodes/case_Node.py
+++ src/nodes/case_Node.py
@@ -12,9 +12,58 @@ class CaseNode:
             response=self.llm.invoke(system_message)
             print(response)
             return {"case":{"title":response.content}}
+    def ipc_sections_creation(self,state:CaseState):
+        if "topic" in state and state["topic"]:
+            prompt="you are an experienced judge with the knowledge of all the ipc section so return me all the ipc that the criminal will face for the crime of {topic} or none if there's no."
+            system_message=prompt.format(topic=state["topic"])
+            response=self.llm.invoke(system_message)
+            return {
+                "charges": {
+                    "sections": response.content
+                }
+            }
     def charges_creation(self,state:CaseState):
         if "topic" in state and state["topic"]:
             system_prompt= """You are an experienced indian judge which have knowledge of all the sections according to the {topic}"""
             system_message=system_prompt.format(topic=state["topic"])
             response=self.llm.invoke(system_message)
-            return {"case":{"title":state['case']['title'],"content":response.content}}
+            return {
+                "case": {
+                    "title": state["case"]["title"],
+                    "content": response.content
+                },
+                "charges": state.get("charges", {})
+            }
+    def final_sentence(self, state: CaseState):
+        if "topic" in state and state["topic"]:
+            topic = state["topic"]
+            case_title = state["case"]["title"]
+            case_content = state["case"]["content"]
+            ipc_sections = state["charges"]["sections"]
+
+        system_prompt = f"""
+You are the final judge.
+
+Topic: {topic}
+
+Case Title: {case_title}
+
+Case Discussion: {case_content}
+
+IPC Sections Applied: {ipc_sections}
+
+Now deliver the FINAL JUDGEMENT including:
+
+- Final sentencing and punishment
+"""
+
+        response = self.llm.invoke(system_prompt)
+
+        return {
+            "case": {
+                "title": case_title,
+                "content": case_content
+            },
+            "charges": state.get("charges", {}),
+            "final_sentence": response.content
+        }
